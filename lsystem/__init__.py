e581e0a682c8ce06e9511b24ecf6b28c0ea5bb4b
Austin Gill <Notgnoshi@gmail.com>
2020-08-30 14:13:59 -0500

    Remove previous attempts at defining the interpreter

--- lsystem/__init__.py
+++ lsystem/__init__.py
@@ -1,3 +1,2 @@
 from .grammar import LSystemGrammar, RuleMapping, Token
-from .graphics import *
 from .turtle import Turtle
--- lsystem/interpreter.py
+++ lsystem/interpreter.py
@@ -1,4 +1,3 @@
-from .graphics import GraphicsWriter
 from .turtle import Turtle
 
 
@@ -8,9 +7,4 @@ class LSystemInterpeter:
     Will need to carefully define the L-System language.
     Note that https://github.com/Objelisks/lsystembot uses placebo letters that don't correspond to
     drawing primitives, but still allow for grammatical transformations.
-
-    Will need to carefully define the format handed off to GraphicsWriter objects. I think the
-    interpreter should stream the line segments (along with associated metadata like thickness and
-    color) to the graphics writers, and let the graphics writers decide if they stream to their
-    respective outputs, or if they cache the results and perform additional operations.
     """
