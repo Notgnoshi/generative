980c294f3f347d9e6c0d2d89ce19074acf174eba
Austin Gill <Notgnoshi@gmail.com>
2020-09-05 13:03:36 -0500

    Implement basic interpreter
    TODO: Add configuration for step size and angle.
    TODO: Test and visualize!
    TODO: Add more/better command sets.

--- lsystem/interpreter.py
+++ lsystem/interpreter.py
@@ -1,9 +1,12 @@
 import io
+import logging
 from typing import Iterable, NewType
 
 from shapely.geometry import LineString
 
 from .turtle import Turtle
+
+logger = logging.getLogger(__name__)
 
 Token = NewType("Token", str)
 Tokens = Iterable[Token]
@@ -24,14 +27,92 @@ class LSystemInterpeter:
         if commandset not in self.commandsets:
             raise ValueError(f"{commandset=} not in {self.commandsets}.")
         self.commandset = commandset
+        self.turtle = Turtle()
+        # TODO: Make these configurable.
+        self.stepsize = 1.0
+        self.angle = 30  # deg
+        self.drawing = True
+        self.active_line = []
+        self.stack = []
 
-    def tokenize(self, input: io.TextIOWrapper) -> Tokens:
+    def tokenize(self, commands: io.TextIOWrapper) -> Tokens:
         """Tokenize the given input using the configured commandset."""
-        return []
+        if self.commandset == "default":
+            return self._tokenize_default(commands)
+        raise ValueError(f"tokenize not supported (yet) for '{self.commandset}'")
 
-    def interpret(self, tokens: Tokens):
+    @staticmethod
+    def _tokenize_default(commands: io.TextIOWrapper) -> Tokens:
+        # The default set of commands are each a single character.
+        # So tokenizing is really easy. Yay.
+        while True:
+            chunk = commands.read(256)
+            if not chunk:
+                break
+            for char in chunk:
+                yield char
+
+    def interpret(self, tokens: Tokens) -> Lines:
         """Interpret the given tokens as 3D Turtle commands."""
-        return []
+        for token in tokens:
+            self._interpret_t(token)
+            # Record the turtle path
+            if self.drawing:
+                self.active_line.append(self.turtle.position)
+            # We stopped drawing, and we have a line fragment to pass on.
+            elif len(self.active_line) > 1:
+                line = LineString(self.active_line)
+                logger.debug("Finished drawing line.")
+                self.active_line = []
+                yield line
+        if len(self.active_line) > 1:
+            line = LineString(self.active_line)
+            logger.debug("Ran out of tokens before line finished.")
+            return line
+
+    def _interpret_t(self, token):
+        logger.debug(f"Interpreting {token=}")
+        if self.commandset == "default":
+            self._interpret_t_default(token)
+        else:
+            raise ValueError(f"commandset '{self.commandset}' unsupported")
+
+    def _interpret_t_default(self, token: Token):
+        if token in {"F", "G"}:
+            self.turtle.forward(self.stepsize)
+            self.drawing = True
+        elif token in {"f", "g"}:
+            self.turtle.forward(self.stepsize)
+            self.drawing = False
+        elif token == "-":
+            self.turtle.yaw(-self.angle)
+        elif token == "+":
+            self.turtle.yaw(+self.angle)
+        elif token == "v":
+            self.turtle.pitch(-self.angle)
+        elif token == "^":
+            self.turtle.pitch(+self.angle)
+        elif token == "<":
+            self.turtle.roll(-self.angle)
+        elif token == ">":
+            self.turtle.roll(+self.angle)
+        elif token == "|":
+            # TODO: Determine if we should also roll 180deg.
+            self.turtle.yaw(180)
+        elif token == "d":
+            self.drawing = True
+        elif token == "D":
+            self.drawing = False
+        elif token == "[":
+            self.stack.append((self.turtle.position, self.turtle.rotation))
+            logger.debug("pushing turtle position, orientation.")
+        elif token == "]":
+            logger.debug("popping turtle position, orientation.")
+            if not self.stack:
+                logger.warning("Stack empty. Can't pop.")
+            else:
+                self.turtle.position, self.turtle.rotation = self.stack.pop()
+            self.drawing = False
 
     def serialize(self, lines: Lines, output: io.TextIOWrapper, format: str):
         """Serialize the turtle's path in the given format.
@@ -44,3 +125,5 @@ class LSystemInterpeter:
         """
         if format != "wkt":
             raise ValueError(f"{format=} is unsupported. Use 'wkt'")
+        for line in lines:
+            output.write(line.wkt + "\n")
