796f5c263b1356e873830255aea3699228631cc5
Austin Gill <Notgnoshi@gmail.com>
2021-02-07 11:55:32 -0600

    Eliminate infernal d,e,a,t,h, ,b,y, ,c,o,m,m,a
    * Add tests around single-character-token rule parsing
    * Support whitespace and/or comma-separated lists
      Requires ',' be an invalid token (that's a good thing)
    * Add parser support for single-character tokens
    
    Closes #32, closes #2

--- generative/lsystem/production.py
+++ generative/lsystem/production.py
@@ -6,63 +6,14 @@ from pyparsing import (
     OneOrMore,
     Optional,
     ParserElement,
+    White,
     Word,
+    alphanums,
     delimitedList,
-    oneOf,
     pyparsing_common,
 )
 
 from .grammar import RuleMapping, Token, TokenName
-
-ParserElement.enablePackrat()
-
-COLON = Literal(":")
-LESS_THAN = Literal("<")
-GREATER_THAN = Literal(">")
-ARROW = Literal("->")
-LEFT_PAREN = Literal("(")
-RIGHT_PAREN = Literal(")")
-
-PITCH_UP = Literal("^")
-PITCH_DOWN = Literal("v")
-# TODO: Does the dual uses of '<' and '>' pose any parsing problems?
-ROLL_CCW = Literal("<")
-ROLL_CW = Literal(">")
-YAW_LEFT = Literal("-")
-YAW_RIGHT = Literal("+")
-PUSH_STACK = Literal("[")
-POP_STACK = Literal("]")
-FLIP_DIRECTION = Literal("|")
-
-_l_token = (
-    pyparsing_common.identifier
-    | PITCH_UP
-    | PITCH_DOWN
-    | ROLL_CCW
-    | ROLL_CW
-    | YAW_LEFT
-    | YAW_RIGHT
-    | PUSH_STACK
-    | POP_STACK
-    | FLIP_DIRECTION
-)
-
-_l_probability = pyparsing_common.real
-_l_rule_lhs = (
-    Optional(_l_token.setResultsName("left_context") + LESS_THAN)
-    + _l_token.setResultsName("lhs")
-    + Optional(GREATER_THAN + _l_token.setResultsName("right_context"))
-)
-_l_rule_rhs = delimitedList(_l_token)
-_l_rule = (
-    _l_rule_lhs
-    + Optional(COLON + _l_probability.setResultsName("probability"))
-    + ARROW
-    + _l_rule_rhs.setResultsName("rhs")
-)
-
-_l_tokens = delimitedList(_l_token)
-_l_ignore = Literal("#ignore") + Optional(COLON) + _l_tokens.setResultsName("ignore")
 
 
 class RuleParser:
@@ -81,34 +32,90 @@ class RuleParser:
         The production rules have the format
 
             [left_context <] lhs [> right_context] [: probability] -> rhs[,rhs[...]]
-
-    TODO: Provide an alternate parser that works on single-character tokens to avoid the infernal
-    death by comma.
     """
 
-    def __init__(self):
+    def __init__(self, long_tokens=False):
         """Create a rule parser.
 
         Parsing rule after rule will construct the RuleParser.rule and RuleParser.ignore members.
+
+        :param long_tokens: Whether to support multiple character tokens. Requires the tokens be
+            comma or whitespace (or both) separated. Defaults to False.
         """
         self.rules: MultiDict[TokenName, RuleMapping] = MultiDict()
         self.ignore: Set[TokenName] = set()
+        self.long_tokens = long_tokens
+
+    @staticmethod
+    def __get_grammars(long_tokens: bool):
+        """Get the grammars for parsing rules and ignore lists.
+
+        :param long_tokens: Whether the tokens must be delimited to support long tokens.
+        """
+        ParserElement.enablePackrat()
+
+        COLON = Literal(":")
+        LESS_THAN = Literal("<")
+        GREATER_THAN = Literal(">")
+        ARROW = Literal("->")
+
+        PITCH_UP = Literal("^")
+        PITCH_DOWN = Literal("v")
+        ROLL_CCW = Literal("<")
+        ROLL_CW = Literal(">")
+        YAW_LEFT = Literal("-")
+        YAW_RIGHT = Literal("+")
+        PUSH_STACK = Literal("[")
+        POP_STACK = Literal("]")
+        FLIP_DIRECTION = Literal("|")
+
+        token = (
+            (Word(alphanums) if long_tokens else Word(alphanums, min=1, exact=1))
+            | PITCH_UP
+            | PITCH_DOWN
+            | ROLL_CCW
+            | ROLL_CW
+            | YAW_LEFT
+            | YAW_RIGHT
+            | PUSH_STACK
+            | POP_STACK
+            | FLIP_DIRECTION
+        )
+
+        probability = pyparsing_common.real
+        rule_lhs = (
+            Optional(token.setResultsName("left_context") + LESS_THAN)
+            + token.setResultsName("lhs")
+            + Optional(GREATER_THAN + token.setResultsName("right_context"))
+        )
+        rule_rhs = delimitedList(token, delim=White()) if long_tokens else OneOrMore(token)
+        rule = (
+            rule_lhs
+            + Optional(COLON + probability.setResultsName("probability"))
+            + ARROW
+            + rule_rhs.setResultsName("rhs")
+        )
+
+        tokens = delimitedList(token, delim=White()) if long_tokens else OneOrMore(token)
+        ignore = Literal("#ignore") + Optional(COLON) + tokens.setResultsName("ignore")
+
+        return rule, ignore
 
     def _parse(self, rule: str):
         """Parse the given rule into textual tokens."""
+        rule_grammar, ignore_grammar = self.__get_grammars(self.long_tokens)
+        rule = rule.replace(",", " ")
         rule = rule.strip()
         if rule.startswith("#"):
-            return _l_ignore.parseString(rule)
+            return ignore_grammar.parseString(rule)
         # NOTE: Expanding this to parametric grammars is nontrivial.
-        return _l_rule.parseString(rule)
+        return rule_grammar.parseString(rule)
 
     def parse(self, rule: str) -> Tuple[Token, RuleMapping]:
         """Parse the given rule into rhs -> production mappings.
 
         As a bit of a terrible design, ignore token lists will be parsed and added to
-        RuleParser.ignore. But the rhs -> production mappings will be created and returned.
-
-        TODO: Find a better design.
+        RuleParser.ignore. But the rhs -> production mappings will still be created and returned.
         """
         results = self._parse(rule)
 
