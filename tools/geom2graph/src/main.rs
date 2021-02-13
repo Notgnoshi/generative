3756b025d4e5956b4662c4ecbdd319715e9bf13b
Austin Gill <Notgnoshi@gmail.com>
2021-02-13 17:35:46 -0600

    Add logging

--- tools/geom2graph/src/main.rs
+++ tools/geom2graph/src/main.rs
@@ -1,8 +1,8 @@
-use structopt::StructOpt;
-
+use log::{debug, error, info, trace, warn};
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -10,9 +10,13 @@ use std::path::PathBuf;
     about = "A CLI application to convert geometries into a graph data structure."
 )]
 struct CmdLineOptions {
-    /// Increase output verbosity.
+    /// Silence all logging.
     #[structopt(short, long)]
-    verbose: bool,
+    quiet: bool,
+
+    /// Increase output verbosity.
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: usize,
 
     /// Input file to read from. Defaults to stdin.
     #[structopt(short, long, parse(from_os_str))]
@@ -28,9 +32,15 @@ fn get_writer(output: Option<PathBuf>) -> BufWriter<Box<dyn Write>> {
     match output {
         Some(path) => match File::create(&path) {
             Err(why) => panic!("couldn't create {} because: {}", path.display(), why),
-            Ok(file) => BufWriter::new(Box::new(file)),
+            Ok(file) => {
+                trace!("Writing to {}", path.display());
+                BufWriter::new(Box::new(file))
+            }
         },
-        None => BufWriter::new(Box::new(std::io::stdout())),
+        None => {
+            trace!("Writing to stdout");
+            BufWriter::new(Box::new(std::io::stdout()))
+        }
     }
 }
 
@@ -39,17 +49,27 @@ fn get_reader(input: Option<PathBuf>) -> BufReader<Box<dyn Read>> {
     match input {
         Some(path) => match File::open(&path) {
             Err(why) => panic!("couldn't read {} because: {}", path.display(), why),
-            Ok(file) => BufReader::new(Box::new(file)),
+            Ok(file) => {
+                trace!("Reading from {}", path.display());
+                BufReader::new(Box::new(file))
+            }
         },
-        None => BufReader::new(Box::new(std::io::stdin())),
+        None => {
+            trace!("Reading from stdin");
+            BufReader::new(Box::new(std::io::stdin()))
+        }
     }
 }
 
 fn main() {
     let args = CmdLineOptions::from_args();
-    if args.verbose {
-        println!("{:#?}", args);
-    }
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(args.quiet)
+        .verbosity(args.verbose)
+        .init()
+        .unwrap();
+
     let mut writer = get_writer(args.output);
     let reader = get_reader(args.input);
 
@@ -58,7 +78,7 @@ fn main() {
 
     for line in reader.lines() {
         if let Ok(geom) = line {
-            println!("{}", geom);
+            trace!("Read: {}", geom);
         }
     }
 }
