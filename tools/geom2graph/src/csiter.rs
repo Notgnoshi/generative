5845c495c13efbf4efca5f96fa547f25f227e9c0
Austin Gill <Notgnoshi@gmail.com>
2021-02-14 16:54:24 -0600

    Split apart CoordSeqIter tests

--- tools/geom2graph/src/csiter.rs
+++ tools/geom2graph/src/csiter.rs
@@ -1,20 +1,33 @@
-struct CoordSeqIterator<'c> {
+use geos::Geom;
+
+// TODO: Rename to PointIterator
+pub struct CoordSeqIterator<'c> {
     index: usize,
     coords: geos::CoordSeq<'c>,
 }
 
 impl<'c> CoordSeqIterator<'c> {
+    // TODO: Is there a way to say this one is private?
     fn new(cs: geos::CoordSeq) -> CoordSeqIterator {
         CoordSeqIterator {
             index: 0,
             coords: cs,
         }
     }
+
+    fn new_from_geometry(geom: geos::Geometry) -> CoordSeqIterator {
+        // TODO: This only works for select geometry types. Need to match on the GeometryType
+        // Will require recursion to create an iterator for GeometryCollections
+        CoordSeqIterator {
+            index: 0,
+            coords: geom.get_coord_seq().expect("Failed to create CoordSeq from Geometry"),
+        }
+    }
 }
 
 /// Turn a CoordSeq into an iterator of geos::GeometryTypes::Points.
 impl<'c> Iterator for CoordSeqIterator<'c> {
-    type Item = geos::Geometry<'c>;
+    type Item = geos::Geometry<'c>; // TODO: Is there a way to specify that this is a Point?
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.index >= self.coords.size().unwrap() {
@@ -49,64 +62,20 @@ impl<'c> Iterator for CoordSeqIterator<'c> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::wkio::GatIterator;
-    use geos::Geom;
 
-
-    // TODO: Split this into different tests, and add a new one that tests whitespace and garbage
-    // skipping.
     #[test]
-    fn test_get_coords() {
-        // NOTE: Polygon's MUST be closed. Geos won't implicitly close them for you.
-        let wkt = "POINT (1 2)  \t \nasdf\n   LINESTRING(1 2, 3 4)    \n POLYGON((0 0, 1 1, 2 2, 3 3, 4 4, 0 0), (0 0, 1 1, 2 2, 0 0))";
-        let mut geoms = crate::wkio::WktDeserializer::from_str(wkt);
+    fn test_point_cs() {
+        let geom = geos::Geometry::new_from_wkt("POINT(1 2)").expect("Failed to create POINT");
+        // TODO: This should be handled by CoordSeqIterator
+        let coords = geom.get_coord_seq().expect("Failed to get POINT coord seq");
+        assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
+        assert_eq!(coords.size(), Ok(1));
 
-        {
-            let point = geoms.next().expect("Failed to parse POINT");
-            let coords = point.get_coord_seq().expect("Couldn't get POINT coord seq");
-            assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
-            assert_eq!(coords.size(), Ok(1));
-
-            let coords = CoordSeqIterator::new(coords);
-            // Test the iterable interface.
-            for coord in coords {
-                assert_eq!(coord.geometry_type(), geos::GeometryTypes::Point);
-
-                let x = coord.get_x().expect("Failed to get X");
-                let y = coord.get_y().expect("Failed to get Y");
-                let z = coord.get_z().expect("Failed to get Z");
-
-                assert_eq!(x, 1.0);
-                assert_eq!(y, 2.0);
-                assert_eq!(z, 0.0);
-
-                let c = coord.get_num_coordinates();
-                assert_eq!(c, Ok(1));
-
-                let dim = coord.get_coordinate_dimension();
-                assert_eq!(dim, Ok(geos::Dimensions::ThreeD));
-
-                let dim = coord.get_num_dimensions();
-                assert_eq!(dim, Ok(0)); // WTF?
-
-                // BUG: This only checks the first two coordinates.
-                assert!(coord == geos::Geometry::new_from_wkt("POINT Z (1 2 999999999)").unwrap());
-            }
-        }
-        {
-            let line = geoms.next().expect("Failed to parse LINESTRING");
-            let coords = line
-                .get_coord_seq()
-                .expect("Couldn't get LINESTRING coord seq");
-            assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
-            assert_eq!(coords.size(), Ok(2));
-
-            // TODO: Why does _this_ have to be mutable when you can loop over the coords just
-            // fine?
-            let mut coords = CoordSeqIterator::new(coords);
-
-            let coord = coords.next().expect("Failed to get first point");
+        let coords = CoordSeqIterator::new(coords);
+        // Test the iterable interface.
+        for coord in coords {
             assert_eq!(coord.geometry_type(), geos::GeometryTypes::Point);
+
             let x = coord.get_x().expect("Failed to get X");
             let y = coord.get_y().expect("Failed to get Y");
             let z = coord.get_z().expect("Failed to get Z");
@@ -115,39 +84,95 @@ mod tests {
             assert_eq!(y, 2.0);
             assert_eq!(z, 0.0);
 
-            let coord = coords.next().expect("Failed to get first point");
-            assert_eq!(coord.geometry_type(), geos::GeometryTypes::Point);
-            let x = coord.get_x().expect("Failed to get X");
-            let y = coord.get_y().expect("Failed to get Y");
-            let z = coord.get_z().expect("Failed to get Z");
+            let c = coord.get_num_coordinates();
+            assert_eq!(c, Ok(1));
 
-            assert_eq!(x, 3.0);
-            assert_eq!(y, 4.0);
-            assert_eq!(z, 0.0);
-        }
-        {
-            let poly = geoms.next().expect("Failed to parse POLYGON");
-            let exterior = poly
-                .get_exterior_ring()
-                .expect("Couldn't get exterior ring");
-            let coords = exterior
-                .get_coord_seq()
-                .expect("Couldn't get POLYGON coord seq");
+            let dim = coord.get_coordinate_dimension();
+            assert_eq!(dim, Ok(geos::Dimensions::ThreeD));
 
-            assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
-            assert_eq!(coords.size(), Ok(6));
+            let dim = coord.get_num_dimensions();
+            assert_eq!(dim, Ok(0)); // WTF?
 
-            let num_holes = poly
-                .get_num_interior_rings()
-                .expect("Couldn't get num holes");
-            assert_eq!(num_holes, 1);
-
-            let hole = poly
-                .get_interior_ring_n(0)
-                .expect("Couldn't get first interior hole");
-            let coords = hole.get_coord_seq().expect("Couldn't get hole coord seq");
-            assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
-            assert_eq!(coords.size(), Ok(4));
+            // BUG: This only checks the first two coordinates.
+            assert!(coord == geos::Geometry::new_from_wkt("POINT Z (1 2 999999999)").unwrap());
         }
     }
+
+    #[test]
+    fn test_linestring_cs() {
+        let geom = geos::Geometry::new_from_wkt("LINESTRING(1 2, 3 4)")
+            .expect("Failed to create LINESTRING");
+        let coords = geom
+            .get_coord_seq()
+            .expect("Couldn't get LINESTRING coord seq");
+        assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
+        assert_eq!(coords.size(), Ok(2));
+
+        // TODO: Why does _this_ have to be mutable when you can loop over the coords just fine?
+        let mut coords = CoordSeqIterator::new(coords);
+
+        let coord = coords.next().expect("Failed to get first point");
+        assert_eq!(coord.geometry_type(), geos::GeometryTypes::Point);
+        let x = coord.get_x().expect("Failed to get X");
+        let y = coord.get_y().expect("Failed to get Y");
+        let z = coord.get_z().expect("Failed to get Z");
+
+        assert_eq!(x, 1.0);
+        assert_eq!(y, 2.0);
+        assert_eq!(z, 0.0);
+
+        let coord = coords.next().expect("Failed to get first point");
+        assert_eq!(coord.geometry_type(), geos::GeometryTypes::Point);
+        let x = coord.get_x().expect("Failed to get X");
+        let y = coord.get_y().expect("Failed to get Y");
+        let z = coord.get_z().expect("Failed to get Z");
+
+        assert_eq!(x, 3.0);
+        assert_eq!(y, 4.0);
+        assert_eq!(z, 0.0);
+    }
+
+    #[test]
+    fn test_polygon_cs() {
+        let geom = geos::Geometry::new_from_wkt(
+            "POLYGON((0 0, 1 1, 2 2, 3 3, 4 4, 0 0), (0 0, 1 1, 2 2, 0 0))",
+        )
+        .expect("Failed to create POLYGON");
+        let exterior = geom
+            .get_exterior_ring()
+            .expect("Couldn't get exterior ring");
+        let coords = exterior
+            .get_coord_seq()
+            .expect("Couldn't get POLYGON coord seq");
+
+        assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
+        assert_eq!(coords.size(), Ok(6));
+
+        let num_holes = geom
+            .get_num_interior_rings()
+            .expect("Couldn't get num holes");
+        assert_eq!(num_holes, 1);
+
+        let hole = geom
+            .get_interior_ring_n(0)
+            .expect("Couldn't get first interior hole");
+        let coords = hole.get_coord_seq().expect("Couldn't get hole coord seq");
+        assert_eq!(coords.dimensions(), Ok(geos::CoordDimensions::TwoD));
+        assert_eq!(coords.size(), Ok(4));
+    }
+
+    #[test]
+    fn test_linearring_cs() {}
+
+    #[test]
+    fn test_multipoint_cs() {}
+
+    #[test]
+    fn test_multilinestring_cs() {}
+
+    #[test]
+    fn test_multipolygon_cs() {}
+
+    #[test]
+    fn test_geometrycollection_cs() {}
 }
