faf46f4db231835600e9a59e01b72f48ca3b4015
Austin Gill <Notgnoshi@gmail.com>
2021-02-13 23:59:32 -0600

    Start playing with the KD tree

--- tools/geom2graph/src/main.rs
+++ tools/geom2graph/src/main.rs
@@ -1,4 +1,5 @@
-//use log::{debug, error, info, trace, warn};
+use kdtree::distance::squared_euclidean;
+use kdtree::KdTree;
 use std::io::Write;
 
 mod cmdline;
@@ -19,7 +20,24 @@ fn main() {
     writeln!(&mut writer, "sample output").expect("Couldn't write?!");
     writer.flush().unwrap();
 
+    // Scalar type, tag type, point type.
+    let mut tree = KdTree::<f64, usize, [f64; 3]>::new(3);
+
+    // An example.
+    let point: [f64; 3] = [0.0, 0.1, 0.2];
+    let data: usize = 0xdeadbeef;
+    tree.add(point, data).unwrap();
+
+    let query = [0.1, 0.1, 0.2];
+    let _result = tree.within(&query, 0.1, &squared_euclidean).unwrap();
+
+    // TODO: The geo::Geometry types are 2D only, so go back to using wkt::Geometry
     for _geom in deserializer {
-        // TODO: Do something useful with the geometries.
+        // for prev, current in pairwise_points(geom) (implicitly convert to 3D, some edge cases
+        // for closed geometries) TODO: How to handle GeometryCollections?
+        //      lookup point in tree
+        //      insert point in tree if it doesn't exist
+        //      add point to graph
+        //      mark current point as adjacent to previous.
     }
 }
