87aebf656ac253ac50da438e91997deb1bc62421
Austin Gill <Notgnoshi@gmail.com>
2021-02-20 16:07:30 -0600

    Initial project setup
    Setup cmake targets:
    * Dependencies
    * Tests
    * geom2graph application
    * geom2graph_lib object library (a library in name only. It's just
      a name for the set of object files)
    * Doxygen documentation (will likely need further customization)

---
+++ tools/geom2graph/include/geom2graph/foo.h
@@ -0,0 +1,2 @@
+/// A placeholder library function to get the tests setup.
+int broken_add(int a, int b);
---
+++ tools/geom2graph/src/geom2graph/foo.cpp
@@ -0,0 +1,6 @@
+#include "geom2graph/foo.h"
+
+int broken_add(int a, int b)
+{
+    return a + b + 1;
+}
