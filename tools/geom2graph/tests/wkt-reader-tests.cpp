24ad16080b2726fb60f7a1a9d43f230526cd6d40
Austin Gill <Notgnoshi@gmail.com>
2021-02-21 22:53:52 -0600

    Fix bug with garbage WKT input at the end of a stream

--- tools/geom2graph/tests/wkt-reader-tests.cpp
+++ tools/geom2graph/tests/wkt-reader-tests.cpp
@@ -15,9 +15,9 @@ static std::unique_ptr<geos::geom::Geometry> from_wkt(const std::string& wkt)
     try
     {
         return reader.read(wkt);
-    }
-    catch (geos::io::ParseException& e)
+    } catch (geos::io::ParseException& e)
     {
+        std::cerr << "Failed to parse '" << wkt << "'" << std::endl;
         return nullptr;
     }
 }
@@ -95,6 +95,54 @@ TEST(WktReaderTests, TestMultipleElements)
     EXPECT_EQ(iter, geometries.end());
 }
 
+TEST(WKTReaderTests, TestExtraNewlines)
+{
+    std::istringstream input{"POINT(0 0)\n\nPOINT(1 1)"};
+    auto geometries = geom2graph::WKTReader(input);
+    size_t i = 0;
+    for (const auto& geometry : geometries)
+    {
+        i++;
+    }
+    ASSERT_EQ(i, 2);
+}
+
+TEST(WKTReaderTests, TestSkipsGarbage)
+{
+    std::istringstream input{"POINT(0 0)\nNOT A POINT\nPOINT(1 1)"};
+    auto geometries = geom2graph::WKTReader(input);
+    size_t i = 0;
+    for (const auto& geometry : geometries)
+    {
+        i++;
+    }
+    ASSERT_EQ(i, 2);
+}
+
+TEST(WKTReaderTests, TestEndsWithGarbage)
+{
+    std::istringstream input{"POINT(0 0)\nNOT A POINT\nPOINT(1 1)\nNOT A POINT"};
+    auto geometries = geom2graph::WKTReader(input);
+    size_t i = 0;
+    for (const auto& geometry : geometries)
+    {
+        i++;
+    }
+    ASSERT_EQ(i, 2);
+}
+
+TEST(WKTReaderTests, TestEndsWithNewline)
+{
+    std::istringstream input{"POINT(0 0)\nNOT A POINT\nPOINT(1 1)\n"};
+    auto geometries = geom2graph::WKTReader(input);
+    size_t i = 0;
+    for (const auto& geometry : geometries)
+    {
+        i++;
+    }
+    ASSERT_EQ(i, 2);
+}
+
 TEST(WktReaderTests, TestRangeLoop)
 {
     std::istringstream input_stream{"POINT(0 0)\nPOINT(1 2)"};
@@ -110,6 +158,63 @@ TEST(WktReaderTests, TestRangeLoop)
         e++;
     }
     EXPECT_EQ(e, expected.size()) << "Expected to loop over " << expected.size() << " geometries";
+}
+
+template<typename Iterator>
+static std::string join(Iterator begin, Iterator end, char separator = ' ')
+{
+    std::ostringstream out;
+    if (begin != end)
+    {
+        out << *begin++;
+        for (; begin != end; ++begin)
+        {
+            out << separator << *begin;
+        }
+    }
+    return out.str();
+}
+
+TEST(WktReaderTests, TestDuplicateGeometry)
+{
+    // Intentionally add some extra newlines
+    std::vector<std::string> wkt = {
+        "MULTIPOINT((1 2), (3 4))\n\n",
+        "POINT(5 6)",
+    };
+    std::istringstream input{join(wkt.begin(), wkt.end(), '\n')};
+    std::vector<std::unique_ptr<geos::geom::Geometry>> expecteds;
+    expecteds.reserve(wkt.size());
+    std::transform(wkt.begin(),
+                   wkt.end(),
+                   std::back_inserter(expecteds),
+                   [](const std::string& wkt) -> std::unique_ptr<geos::geom::Geometry> {
+                       return std::move(from_wkt(wkt));
+                   });
+    ASSERT_EQ(wkt.size(), expecteds.size());
+    for (const auto& expected : expecteds)
+    {
+        ASSERT_NE(expected, nullptr);
+    }
+
+    geom2graph::WKTReader geometries(input);
+    auto actual = geometries.begin();
+    auto expected = expecteds.begin();
+    for (; actual != geometries.end() && expected != expecteds.end(); ++actual, ++expected)
+    {
+        ASSERT_NE(actual, geometries.end());
+        ASSERT_NE(expected, expecteds.end());
+
+        ASSERT_NE(*actual, nullptr);
+        SCOPED_TRACE("Actual: " + (*actual)->toString());
+        ASSERT_NE(*expected, nullptr);
+        SCOPED_TRACE("Expected: " + (*expected)->toString());
+
+        ASSERT_EQ((*actual)->getGeometryTypeId(), (*expected)->getGeometryTypeId());
+    }
+
+    EXPECT_EQ(actual, geometries.end());
+    EXPECT_EQ(expected, expecteds.end());
 }
 
 TEST(GeosWKTReaderTests, TestValidGeom)
