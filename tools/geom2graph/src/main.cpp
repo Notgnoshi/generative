3fa9239771c773c635d13a84f3c7bca0b35495d6
Austin Gill <Notgnoshi@gmail.com>
2021-02-27 14:31:33 -0600

    Collapse geometry stream into a collection

--- tools/geom2graph/src/main.cpp
+++ tools/geom2graph/src/main.cpp
@@ -7,6 +7,7 @@
 #include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
 
+#include <algorithm>
 #include <string>
 
 static auto s_logger = log4cplus::Logger::getRoot();
@@ -19,10 +20,16 @@ int main(int argc, const char* argv[])
     s_logger.addAppender(appender);
 
     const CmdlineArgs args = CmdlineArgs::parse_args(argc, argv);
-    auto geometries = geom2graph::io::WKTStreamReader(args.input);
+    auto geom_stream = geom2graph::io::WKTStreamReader(args.input);
+
+    // Collapse the input stream, because it doesn't appear that a geos::noding::Noder can only node
+    // a static collection of geometries all at once instead of on-the-fly, which kind of makes
+    // sense, but requires loading the entire stream into memory at once.
+    const auto geometries = geom_stream.collapse();
 
     for (const auto& geometry : geometries)
     {
+        //! @todo The noding process doesn't require flattened geometries.
         LOG4CPLUS_DEBUG(s_logger, "Flattening " << geometry->toString());
         for (const auto& flat_geometry : geom2graph::GeometryFlattener(*geometry))
         {
