19aa27914f9b749034c45d28f54e7bb66c021e1b
Austin Gill <Notgnoshi@gmail.com>
2022-12-26 11:27:44 -0600

    Update examples with the new wkt2svg
    Unfortunately, it seems as though the examples are not _perfectly_
    reproducible, as there's many thousand lines of diffs when I run the
    script twice. It seems though, that the differences are miniscule, which
    is unfortunate.

--- examples/generate-examples.sh
+++ examples/generate-examples.sh
@@ -9,61 +9,76 @@ SOURCE="${BASH_SOURCE[0]}"
 REPO_ROOT="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)/.."
 REPO_ROOT="$(readlink --canonicalize --no-newline "${REPO_ROOT}")"
 
-# Geometry Formats
-$REPO_ROOT/tools/format.py --input $REPO_ROOT/examples/maya-tree-2.wkt --output-format flat --output $REPO_ROOT/examples/maya-tree-2.flat
-$REPO_ROOT/tools/format.py --input-format flat --input $REPO_ROOT/examples/maya-tree-2.flat --output-format wkb --output $REPO_ROOT/examples/maya-tree-2.wkb
-diff <($REPO_ROOT/tools/format.py --input-format wkb --input $REPO_ROOT/examples/maya-tree-2.wkb --output-format wkt) $REPO_ROOT/examples/maya-tree-2.wkt
+cd "$REPO_ROOT"
+cargo build --release
 
-# SVG Generation
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/sierpinski-tree.json |
-    $REPO_ROOT/tools/interpret.py |
-    $REPO_ROOT/tools/project.py --kind=yz |
-    $REPO_ROOT/tools/wkt2svg.py -o $REPO_ROOT/examples/sierpinski-tree.svg
+PATH="$REPO_ROOT/target/release/:$PATH"
 
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/sierpinski-tree.json |
-    $REPO_ROOT/tools/interpret.py |
-    $REPO_ROOT/tools/project.py --kind=pca |
-    $REPO_ROOT/tools/wkt2svg.py -o $REPO_ROOT/examples/sierpinski-tree-pca.svg
+echo "DLA..."
+dla \
+    --seed 461266331856721221 \
+    --seeds 2 \
+    --attraction-distance 10 \
+    --min-move-distance 1 \
+    --stubbornness 10 \
+    --particle-spacing 0.1 |
+    geom2graph --graph2geom |
+    ./tools/project.py --kind I --scale 20 |
+    wkt2svg --output ./examples/diffusion-limited-aggregation/organic.svg
 
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/fractal-plant-1.json |
-    $REPO_ROOT/tools/interpret.py --stepsize=3 --angle=22.5 |
-    $REPO_ROOT/tools/project.py --kind=yz |
-    $REPO_ROOT/tools/wkt2svg.py -o $REPO_ROOT/examples/fractal-plant-1.svg
+echo "Lindenmayer..."
+"$REPO_ROOT/tools/parse-production-rules.py" --config "$REPO_ROOT/examples/sierpinski-tree.json" |
+    "$REPO_ROOT/tools/interpret-lstring.py" |
+    "$REPO_ROOT/tools/project.py" --kind=yz |
+    wkt2svg --output "$REPO_ROOT/examples/sierpinski-tree.svg"
 
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/fractal-plant-3d.json | $REPO_ROOT/tools/interpret.py --stepsize=3 --angle=22.5 >/tmp/plant.wkt
-for projection in pca svd; do
-    $REPO_ROOT/tools/project.py --kind=$projection --scale 10 --input /tmp/plant.wkt | $REPO_ROOT/tools/wkt2svg.py -o $REPO_ROOT/examples/plant-$projection.svg
-done
-
-# Converting the WKT to a graph
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/fractal-plant-1.json |
-    $REPO_ROOT/tools/interpret.py --stepsize=3 --angle=22.5 |
-    $REPO_ROOT/tools/project.py --kind=pca --output $REPO_ROOT/examples/fractal-plant-1.wkt
-
-$REPO_ROOT/tools/geom2graph/build/src/geom2graph \
-    --tolerance=0.001 \
-    --input $REPO_ROOT/examples/fractal-plant-1.wkt \
-    --output $REPO_ROOT/examples/fractal-plant-1.tgf
-
-$REPO_ROOT/tools/parse.py --config $REPO_ROOT/examples/maya-tree-2.json |
-    $REPO_ROOT/tools/interpret.py \
+"$REPO_ROOT/tools/parse-production-rules.py" --config "$REPO_ROOT/examples/maya-tree-2.json" |
+    "$REPO_ROOT/tools/interpret-lstring.py" \
         --stepsize=1 \
         --angle=30 \
-        --output=$REPO_ROOT/examples/maya-tree-2.wkt
+        --output="$REPO_ROOT/examples/maya-tree-2.wkt"
 
-$REPO_ROOT/tools/geom2graph/build/src/geom2graph \
+echo "Projections..."
+"$REPO_ROOT/tools/parse-production-rules.py" --config "$REPO_ROOT/examples/sierpinski-tree.json" |
+    "$REPO_ROOT/tools/interpret-lstring.py" |
+    "$REPO_ROOT/tools/project.py" --kind=pca |
+    wkt2svg --output "$REPO_ROOT/examples/sierpinski-tree-pca.svg"
+
+"$REPO_ROOT/tools/parse-production-rules.py" --config "$REPO_ROOT/examples/fractal-plant-1.json" |
+    "$REPO_ROOT/tools/interpret-lstring.py" --stepsize=3 --angle=22.5 |
+    "$REPO_ROOT/tools/project.py" --kind=yz --output "$REPO_ROOT/examples/fractal-plant-1.wkt"
+"$REPO_ROOT/tools/project.py" --kind=pca --input "$REPO_ROOT/examples/fractal-plant-1.wkt" --output "$REPO_ROOT/examples/fractal-plant-1-pca.wkt"
+wkt2svg --input "$REPO_ROOT/examples/fractal-plant-1.wkt" --output "$REPO_ROOT/examples/fractal-plant-1.svg"
+wkt2svg --input "$REPO_ROOT/examples/fractal-plant-1-pca.wkt" --output "$REPO_ROOT/examples/fractal-plant-1-pca.svg"
+
+echo "Triangulation..."
+point-cloud --seed 9290215150213667180 --points 20 --scale 100 >/tmp/points.wkt
+triangulate </tmp/points.wkt >/tmp/delaunay.wkt
+wkt2svg </tmp/delaunay.wkt >examples/delaunay.svg
+
+echo "Urquhart..."
+urquhart </tmp/points.wkt >/tmp/urquhart.wkt
+wkt2svg </tmp/urquhart.wkt >examples/urquhart.svg
+
+echo "Converting the WKT to a graph..."
+geom2graph \
     --tolerance=0.001 \
-    --input $REPO_ROOT/examples/maya-tree-2.wkt \
-    --output $REPO_ROOT/examples/maya-tree-2.tgf
+    --input "$REPO_ROOT/examples/fractal-plant-1.wkt" \
+    --output "$REPO_ROOT/examples/fractal-plant-1.tgf"
 
-# Generating Random L-Systems
-mkdir -p $REPO_ROOT/examples/random-lsystems
+geom2graph \
+    --tolerance=0.001 \
+    --input "$REPO_ROOT/examples/maya-tree-2.wkt" \
+    --output "$REPO_ROOT/examples/maya-tree-2.tgf"
+
+echo "Generating Random L-Systems..."
+mkdir -p "$REPO_ROOT/examples/random-lsystems"
 for i in $(seq 0 13); do
-    # $REPO_ROOT/tools/geom2graph/build/src/geom2graph --tolerance 1e-3 |  # Use geom2graph round trip to simplify geometries
-    # $REPO_ROOT/tools/geom2graph/build/src/geom2graph --tolerance 1e-3 --graph2geom |
-    jq ".[$i]" $REPO_ROOT/examples/random-lsystems/saved.json |
-    $REPO_ROOT/tools/parse.py -c - -n $(jq ".[$i].iterations" $REPO_ROOT/examples/random-lsystems/saved.json) |
-    $REPO_ROOT/tools/interpret.py -l ERROR -a $(jq ".[$i].angle" $REPO_ROOT/examples/random-lsystems/saved.json) |
-    $REPO_ROOT/tools/project.py --scale $(jq ".[$i].scale" $REPO_ROOT/examples/random-lsystems/saved.json) --kind pca |
-    $REPO_ROOT/tools/wkt2svg.py --output $REPO_ROOT/examples/random-lsystems/random-$i.svg
+    # geom2graph --tolerance 1e-3 |  # Use geom2graph round trip to simplify geometries
+    # geom2graph --tolerance 1e-3 --graph2geom |
+    jq ".[$i]" "$REPO_ROOT/examples/random-lsystems/saved.json" |
+        "$REPO_ROOT/tools/parse-production-rules.py" -c - -n "$(jq ".[$i].iterations" "$REPO_ROOT/examples/random-lsystems/saved.json")" |
+        "$REPO_ROOT/tools/interpret-lstring.py" -l ERROR -a "$(jq ".[$i].angle" "$REPO_ROOT/examples/random-lsystems/saved.json")" |
+        "$REPO_ROOT/tools/project.py" --scale "$(jq ".[$i].scale" "$REPO_ROOT/examples/random-lsystems/saved.json")" --kind pca |
+        wkt2svg --output "$REPO_ROOT/examples/random-lsystems/random-$i.svg"
 done
