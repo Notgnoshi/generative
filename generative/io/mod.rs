935de204234628ee92b082d52d70e99f4b4628e3
Austin Gill <Notgnoshi@gmail.com>
2022-12-30 15:32:22 -0600

    Add TGF graph deserialization

--- generative/io/mod.rs
+++ generative/io/mod.rs
@@ -3,7 +3,9 @@ mod tgf;
 mod wkt;
 
 pub use stdio::{get_input_reader, get_output_writer};
-pub use tgf::{write_graph, GraphFormat};
+// TODO: A read_graph method that takes in the GraphFormat (the same as read_geometries) would
+// require FFI bindings to geom2graph. See: https://github.com/Notgnoshi/generative/issues/130
+pub use tgf::{read_tgf_graph, write_graph, GraphFormat};
 
 pub use self::wkt::{
     read_geometries, read_wkt_geometries, write_geometries, write_wkt_geometries, GeometryFormat,
--- generative/io/tgf.rs
+++ generative/io/tgf.rs
@@ -1,9 +1,11 @@
-use std::io::Write;
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read, Write};
 
 use clap::ValueEnum;
-use geo::{Geometry, Line};
+use geo::{Geometry, Line, Point};
 use petgraph::visit::EdgeRef;
 use petgraph::EdgeType;
+use wkt::TryFromWkt;
 
 use crate::graph::GeometryGraph;
 use crate::io::write_wkt_geometries;
@@ -20,12 +22,12 @@ where
     Direction: EdgeType,
 {
     match format {
-        GraphFormat::Tgf => write_graph_tgf(writer, graph),
-        GraphFormat::Wkt => write_graph_wkt(writer, graph),
+        GraphFormat::Tgf => write_tgf_graph(writer, graph),
+        GraphFormat::Wkt => write_wkt_graph(writer, graph),
     }
 }
 
-fn write_graph_tgf<Direction, W>(mut writer: W, graph: GeometryGraph<Direction>)
+fn write_tgf_graph<Direction, W>(mut writer: W, graph: GeometryGraph<Direction>)
 where
     W: Write,
     Direction: EdgeType,
@@ -51,7 +53,7 @@ where
     }
 }
 
-fn write_graph_wkt<Direction, W>(writer: W, graph: GeometryGraph<Direction>)
+fn write_wkt_graph<Direction, W>(writer: W, graph: GeometryGraph<Direction>)
 where
     W: Write,
     Direction: EdgeType,
@@ -61,4 +63,183 @@ where
         .map(|e| Line::new(graph[e.source()], graph[e.target()]))
         .map(Geometry::Line);
     write_wkt_geometries(writer, edges);
+}
+
+fn read_raw_node(line: String) -> Result<(usize, Point), String> {
+    let mut parts = line.split_whitespace();
+
+    let raw_id = if let Some(id) = parts.next() {
+        id
+    } else {
+        return Err(format!(
+            "Failed to parse node: '{}' missing raw node id",
+            line
+        ));
+    };
+
+    let id = if let Ok(id) = raw_id.parse::<usize>() {
+        id
+    } else {
+        return Err(format!("Failed to parse ID from: '{}'", line));
+    };
+
+    let raw_label = parts.fold(String::new(), |a, b| a + b + "\n");
+    let label = if let Ok(label) = Point::try_from_wkt_str(&raw_label) {
+        label
+    } else {
+        return Err(format!(
+            "Failed to parse node: {}: label '{}' as WKT POINT",
+            id, raw_label
+        ));
+    };
+
+    Ok((id, label))
+}
+
+// TODO: Read edge weights
+fn read_raw_edge(line: String) -> Result<(usize, usize), String> {
+    let mut parts = line.split_whitespace();
+
+    let raw_source = if let Some(source) = parts.next() {
+        source
+    } else {
+        return Err(format!("Failed to parse raw source ID from '{}'", line));
+    };
+    let raw_target = if let Some(target) = parts.next() {
+        target
+    } else {
+        return Err(format!("Failed to parse raw target ID from '{}'", line));
+    };
+
+    let source = if let Ok(source) = raw_source.parse::<usize>() {
+        source
+    } else {
+        return Err(format!(
+            "Failed to convert '{}' to source node ID",
+            raw_source
+        ));
+    };
+    let target = if let Ok(target) = raw_target.parse::<usize>() {
+        target
+    } else {
+        return Err(format!(
+            "Failed to convert '{}' to target node ID",
+            raw_target
+        ));
+    };
+
+    Ok((source, target))
+}
+
+pub fn read_tgf_graph<Direction, R>(reader: R) -> GeometryGraph<Direction>
+where
+    R: Read,
+    Direction: EdgeType,
+{
+    let mut lines = BufReader::new(reader).lines();
+    let mut raw_to_real = HashMap::new();
+    let mut graph = GeometryGraph::<Direction>::default();
+
+    // Read the nodes
+    loop {
+        let current_line = if let Some(line) = lines.next() {
+            line.unwrap()
+        } else {
+            break;
+        };
+
+        if current_line.starts_with('#') {
+            break;
+        }
+
+        let (raw_id, label) = match read_raw_node(current_line) {
+            Ok(node) => node,
+            Err(e) => {
+                log::warn!("Failed to parse node: {:?}", e);
+                continue;
+            }
+        };
+
+        // There's no guarantee that the nodes were serialized in strictly increasing order, so we
+        // have to map between petgraph's node indices and the raw node indices given in the TGF.
+        let real_id = graph.add_node(label);
+        raw_to_real.insert(raw_id, real_id);
+    }
+
+    // Read the edges
+    for line in lines {
+        let current_line = line.unwrap();
+        let (raw_source, raw_target) = match read_raw_edge(current_line) {
+            Ok(edge) => edge,
+            Err(e) => {
+                log::warn!("Failed to parse edge: {:?}", e);
+                continue;
+            }
+        };
+
+        let source = raw_to_real.get(&raw_source);
+        let target = raw_to_real.get(&raw_target);
+        match (source, target) {
+            (Some(real_source), Some(real_target)) => {
+                graph.add_edge(*real_source, *real_target, ());
+            }
+            _ => {
+                log::warn!(
+                    "Failed to find node from edge {} -> {} in graph",
+                    raw_source,
+                    raw_target
+                );
+                continue;
+            }
+        }
+    }
+
+    graph
+}
+
+#[cfg(test)]
+mod tests {
+    use petgraph::{Directed, Undirected};
+
+    use super::*;
+
+    #[test]
+    fn test_read_tgf_undirected() {
+        let tgf = b"1\tPOINT(0 0)\n42\tPOINT(2 2)\n#\n1\t42";
+        let graph = read_tgf_graph::<Undirected, _>(&tgf[..]);
+
+        assert_eq!(graph.node_count(), 2);
+        assert_eq!(graph.edge_count(), 1);
+
+        let first = graph.node_weight(0.into());
+        let expected = Point::new(0.0, 0.0);
+        assert_eq!(first, Some(&expected));
+
+        let second = graph.node_weight(1.into());
+        let expected = Point::new(2.0, 2.0);
+        assert_eq!(second, Some(&expected));
+
+        assert!(graph.contains_edge(0.into(), 1.into()));
+        assert!(graph.contains_edge(1.into(), 0.into()));
+    }
+
+    #[test]
+    fn test_read_tgf_directed() {
+        let tgf = b"1\tPOINT(0 0)\n42\tPOINT(2 2)\n#\n1\t42";
+        let graph = read_tgf_graph::<Directed, _>(&tgf[..]);
+
+        assert_eq!(graph.node_count(), 2);
+        assert_eq!(graph.edge_count(), 1);
+
+        let first = graph.node_weight(0.into());
+        let expected = Point::new(0.0, 0.0);
+        assert_eq!(first, Some(&expected));
+
+        let second = graph.node_weight(1.into());
+        let expected = Point::new(2.0, 2.0);
+        assert_eq!(second, Some(&expected));
+
+        assert!(graph.contains_edge(0.into(), 1.into()));
+        assert!(!graph.contains_edge(1.into(), 0.into()));
+    }
 }
