18ea39519be1363a9521916d91c6fb876e36e7d2
Austin Gill <Notgnoshi@gmail.com>
2023-12-27 11:57:27 -0600

    Enable logging from tests

--- generative/lib.rs
+++ generative/lib.rs
@@ -6,3 +6,13 @@ pub mod io;
 pub mod triangulation;
 
 pub use geometry_mut_map::MapCoordsInPlaceMut;
+
+#[cfg(test)]
+#[ctor::ctor]
+fn init_test_logging() {
+    stderrlog::new()
+        .verbosity(log::Level::Trace)
+        .color(stderrlog::ColorChoice::Auto)
+        .init()
+        .expect("Failed to initialize stderrlog");
+}
