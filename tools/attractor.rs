e6808e809f5cee8315609708dde87963f429e792
Austin Gill <Notgnoshi@gmail.com>
2025-10-02 15:20:20 -0500

    Stub out attractor tool

---
+++ tools/attractor.rs
@@ -0,0 +1,25 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Attractor; runs dynamical systems
+#[derive(Debug, Parser)]
+#[clap(name = "attractor", verbatim_doc_comment)]
+struct CmdlineOptions {
+    /// The log level
+    #[clap(short, long, default_value_t = tracing::Level::INFO)]
+    log_level: tracing::Level,
+}
+
+fn main() {
+    let args = CmdlineOptions::parse();
+
+    let filter = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(args.log_level.into())
+        .from_env_lossy();
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_ansi(true)
+        .with_writer(std::io::stderr)
+        .init();
+}
